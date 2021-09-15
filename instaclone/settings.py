4c26f0289c38143e803c16ab54af09d46170302d
k-koech <kelvin.koech@student.moringaschool.com>
2021-09-16 00:44:20 +0300

    heroku deploy

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a@q@5xyqne^18^ywr1y6t#@33g!+9t98cy^+0oilj52&@qa25a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get['DEBUG']
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = os.environ.get['ALLOWED_HOSTS']
 
